f336e7e2e4ad2a7a3999eeca14f9b98bbf68f793
KsenyBond <kseny.bondarchuk@gmail.com>
2019-03-21 00:14:44 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let iterations,
+      result = false,
+      stringArray = [];
+
+  if (!(str.length % 2)) {
+      bracketsConfig.forEach(function (value) {
+          stringArray.push(value[0] + value[1]);
+      });
+
+      iterations = str.length / 2;
+
+      for (let i = 0; i < iterations; i++) {
+          stringArray.forEach(function (value) {
+              str = str.replace(value, '');
+          });
+
+          if (str.length === 0) {
+              result = true;
+          }
+      }
+  }
+
+  return result;
+};
